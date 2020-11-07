dc7a5ee1b5b88a53fec2583e651bb8a9e2380896
Tommaso Siligardi <tomesse@outlook.it>
2020-11-07 19:05:28 +0100

    fino a Handlebars Configuration (escluso)

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express=require('express')//inserisce la libreria di express
-const app = express() //crea un server di expree
+const app = express() //crea un server di express
+
+const handlebars = require('express-handlebars')//carica il modulo di handelbars
+app.set('view engine', 'handlebars')//abilita il nostro programma ad usare Handlebars 
+
 const port = 8080 //porta su cui si avvia il server
+
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    })) //Recupera l'HTML statico da views (index.handelbars) e il body con quelllo che gli passo nel res.render
+
 app.use(express.static('public'))//serve per usare i files css contenuti nella cartella public
-app.get('/', (req, res) => res.send('Hello World !'))
+app.get('/', (req, res) => res.render('main',{layout:'index'})) 
+//res.render(cosa_ci_metto_dentro, {layout: cosa_riempio})
 app.listen(port, () => console.log(`App listening to port ${port}`))
