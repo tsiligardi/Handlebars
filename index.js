2bbdaf4fe5fc5d4b36f1063f413a75f613571c51
Tommaso Siligardi <tomesse@outlook.it>
2020-11-09 20:47:34 +0100

    finita la parte statica da iniziare quella dinamica

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ app.engine('hbs', handlebars({
     layoutsDir: __dirname + '/views/layouts',
     extname: 'hbs',
     defaultLayout: 'index', //indica il layout di default
+    partialsDir: __dirname + '/views/partials/'//indica da dove vado a prendere i partials da inserire
     }));
     app.get('/', (req, res) => {
-    res.render('main',{layout: 'planB'}); //se ometto {layout: 'nomefile'} gli passo quello di default
+    res.render('main',{layout:'index'}); //se ometto {layout: 'nomefile'} gli passo quello di default
     });
 app.listen(port, () => console.log(`App listening to port ${port}`))
