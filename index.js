b2a81d71421daa8876f936512ca6a6b4706e70c3
Tommaso Siligardi <tomesse@outlook.it>
2020-11-09 10:36:22 +0100

    modificata estensione file

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express=require('express')//inserisce la libreria di express
 const app = express() //crea un server di express
 
 const handlebars = require('express-handlebars')//carica il modulo di handelbars
-app.set('view engine', 'handlebars')//abilita il nostro programma ad usare Handlebars 
+app.set('view engine', 'hbs')//app.set('view engine', estensione_file)
 
 const port = 8080 //porta su cui si avvia il server
 
-app.engine('handlebars', handlebars({
+app.engine('hbs', handlebars({
     layoutsDir: __dirname + '/views/layouts',
-    })) //Recupera l'HTML statico da views (index.handelbars) e il body con quelllo che gli passo nel res.render
-
+    extname: 'hbs',
+    })) //esxtname: in questo modo posso indicare l'estensione dei file a cui mi riferisco con handelbars
 app.use(express.static('public'))//serve per usare i files css contenuti nella cartella public
 app.get('/', (req, res) => res.render('main',{layout:'index'})) 
 //res.render(cosa_ci_metto_dentro, {layout: cosa_riempio})
